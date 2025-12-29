4634b2b0aacea4c0e8099754ee178a0d0c2e76ad
Ahmed Nageh <ahmdnajkh71@gmail.com>
2025-12-30 01:13:40 +0200

    Enhance PrimaryController and primary.fxml with player search functionality

--- Tic_Tac_Toe_Server/src/main/java/com/mycompany/tic_tac_toe_server/PrimaryController.java
+++ Tic_Tac_Toe_Server/src/main/java/com/mycompany/tic_tac_toe_server/PrimaryController.java
@@ -10,6 +10,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 
 /**
@@ -25,6 +26,8 @@ public class PrimaryController implements Initializable {
     private Button stopServer;
     @FXML
     private VBox playersList;
+    @FXML
+    private TextField searchPlayerTextField;
 
     /**
      * Initializes the controller class.
